96ffeaa896411defd6396567739cb310ac0c772b
Bagas Irwansyah <bagasirwansyah@yahoo.com>
2022-09-09 17:17:46 +0700

    [add]: handler function for home and about

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+// Home is the homepage handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "This is the home page")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+}
+
+// addValies adds two integers and return the sum
+func addValues(x int, y int) int {
+	return x + y
+}
+
+// main is the main application function
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
+}
