3e227658703efb22ace12258810a53a65ab8f519
Bagas Irwansyah <bagasirwansyah@yahoo.com>
2022-09-11 01:37:26 +0700

    [add]: divide function

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -23,10 +24,28 @@ func addValues(x int, y int) int {
 	return x + y
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValus(100.0, 10.0)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by zero")
+	}
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 10.0, f))
+}
+
+func divideValus(x float32, y float32) (float32, error) {
+	if y == 0 {
+		err := errors.New("Cannot divide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
 // main is the main application function
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
