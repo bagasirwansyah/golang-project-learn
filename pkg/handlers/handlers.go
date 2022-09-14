c4a0a2c9c2164b8a3c5bfa1c8fbf0e1b14edfc56
Bagas Irwansyah <bagasirwansyah@yahoo.com>
2022-09-14 13:53:49 +0700

    [modify]: change file directory

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,15 +1,16 @@
-package main
+package handlers
 
 import (
+	"go-learn-web/pkg/render"
 	"net/http"
 )
 
 // Home is the homepage handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.html")
+	render.RenderTemplate(w, "home.page.html")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.html")
+	render.RenderTemplate(w, "about.page.html")
 }
