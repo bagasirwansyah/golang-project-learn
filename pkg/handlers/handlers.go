11ac3038f43470d979cfc2a9707469e8e5fa2e79
Bagas Irwansyah <bagasirwansyah@yahoo.com>
2022-09-15 12:17:27 +0700

    [add]: templatedata

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"go-learn-web/pkg/config"
+	"go-learn-web/pkg/models"
 	"go-learn-web/pkg/render"
 	"net/http"
 )
@@ -28,10 +29,17 @@ func NewHandlers(r *Repository) {
 
 // Home is the homepage handler
 func (pr *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
 // About is the about page handler
 func (pr *Repository) About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.tmpl")
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again."
+
+	//send the data
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -3,6 +3,7 @@ package render
 import (
 	"bytes"
 	"go-learn-web/pkg/config"
+	"go-learn-web/pkg/models"
 	"html/template"
 	"log"
 	"net/http"
@@ -16,8 +17,12 @@ func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+	return td
+}
+
 // RenderTemplate renders templates using html/template
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
 	var tc map[string]*template.Template
 	if app.UseCache {
 		// get the template cache from the app config
@@ -33,7 +38,9 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 
 	buf := new(bytes.Buffer)
 
-	err := t.Execute(buf, nil)
+	td = AddDefaultData(td)
+
+	err := t.Execute(buf, td)
 	if err != nil {
 		log.Println(err)
 	}
